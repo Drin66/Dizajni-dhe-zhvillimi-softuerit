630b2302812a3b95e1d8d11894ce62c70fa9409d
IZ10na <152287176+IZ10na@users.noreply.github.com>
2025-01-11 11:17:29 +0100

    Add files via upload

---
+++ Cinema/backend/src/domain/Movie.js
@@ -0,0 +1,10 @@
+class Movie {
+    constructor(id, title, description, categoryId) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.categoryId = categoryId;
+    }
+}
+
+export default Movie;
