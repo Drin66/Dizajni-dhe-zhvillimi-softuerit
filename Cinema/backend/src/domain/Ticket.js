a55cef19544c935d6cbef212a90bfeaffc28e27c
Drin Avdiaj <149594472+Drin66@users.noreply.github.com>
2025-01-11 11:18:21 +0100

    Add files via upload

---
+++ Cinema/backend/src/domain/Ticket.js
@@ -0,0 +1,10 @@
+class Ticket {
+    constructor(id, seat, movieId, userId) {
+        this.id = id;
+        this.seat = seat;
+        this.movieId = movieId;
+        this.userId = userId;
+    }
+}
+
+export default Ticket;
