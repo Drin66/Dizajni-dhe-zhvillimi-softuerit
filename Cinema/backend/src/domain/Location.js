0006ab318a3321bf906b775a8237677da190a904
IZ10na <152287176+IZ10na@users.noreply.github.com>
2025-01-12 13:24:25 +0100

    Add files via upload

---
+++ Cinema/backend/src/domain/Location.js
@@ -0,0 +1,10 @@
+class Location {
+    constructor(id, address, city, country) {
+        this.id = id;
+        this.address = address;
+        this.city = city;
+        this.country = country;
+    }
+}
+
+export default Location;
