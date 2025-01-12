3d2b01f389ee94fcb0337da4f72bf9fcbc2044f8
IZ10na <152287176+IZ10na@users.noreply.github.com>
2025-01-12 13:23:39 +0100

    Add files via upload

---
+++ Cinema/backend/src/domain/Announcements.js
@@ -0,0 +1,10 @@
+class Announcement {
+    constructor(id, title, message, date) {
+        this.id = id;
+        this.title = title;
+        this.message = message;
+        this.date = date;
+    }
+}
+
+export default Announcement;
